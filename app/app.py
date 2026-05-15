4345cf6b0b9868f8a712c85fc6ff76d662733d8c
adham <adhamfatene@gmail.com>
2026-05-15 13:46:43 +0100

    Connect Streamlit to Render API

--- app/app.py
+++ app/app.py
@@ -5,13 +5,22 @@ st.title("Waste Detection")
 
 user_input = st.text_input("Enter data")
 
+API_URL = "https://waste-detection-api-2csu.onrender.com/predict"
+
 if st.button("Predict"):
 
     response = requests.post(
-        "http://api:8000/predict",
+        API_URL,
         json={"data": user_input}
     )
 
     result = response.json()
 
     st.success(result["prediction"])
+
+
+if response.status_code == 200:
+    result = response.json()
+    st.success(result["prediction"])
+else:
+    st.error("API error")
